b10fb7237b5349d28d45f046d668c3ed47dc40ff
HorabikJ <horabik.j@gmail.com>
2019-06-20 22:54:05 +0200

    Added registerUser() method.

---
+++ src/main/java/pl/coderslab/javaGym/controller/RegistrationController.java
@@ -0,0 +1,31 @@
+package pl.coderslab.javaGym.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.javaGym.entity.User;
+import pl.coderslab.javaGym.service.UserService;
+
+import javax.validation.Valid;
+
+@RestController
+public class RegistrationController {
+
+    private UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registerUser(@RequestBody @Valid User user) {
+        return userService.findUserByEmail(user.getEmail());
+    }
+
+
+}
