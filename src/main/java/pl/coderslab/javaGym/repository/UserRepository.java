fd4f9225f4bcf6172bf2812bfa3e077eee40539b
HorabikJ <horabik.j@gmail.com>
2019-06-20 22:53:28 +0200

    Added getAllUsersEmails() method.

--- src/main/java/pl/coderslab/javaGym/repository/UserRepository.java
+++ src/main/java/pl/coderslab/javaGym/repository/UserRepository.java
@@ -2,12 +2,18 @@ package pl.coderslab.javaGym.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.javaGym.entity.User;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
+    @Query("SELECT u.email FROM User u")
+    List<String> getAllUsersEmails();
 
 }
