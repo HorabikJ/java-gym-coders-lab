cc3ddbef4d644f4b326a55957bfc55587d98443b
HorabikJ <horabik.j@gmail.com>
2019-06-22 13:46:03 +0200

    Implemented major changes to CustomGlobalExceptionHandler class.

--- src/main/java/pl/coderslab/javaGym/error/CustomGlobalExceptionHandler.java
+++ src/main/java/pl/coderslab/javaGym/error/CustomGlobalExceptionHandler.java
@@ -12,10 +12,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.coderslab.javaGym.error.customException.DomainObjectException;
+import pl.coderslab.javaGym.error.customException.NotAuthenticatedException;
+import pl.coderslab.javaGym.error.customException.ResourceNotFoundException;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,13 +24,14 @@ import java.util.Map;
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(DomainObjectException.class)
-    public ResponseEntity<CustomErrorResponse> customDomainObjectException
+    @ExceptionHandler({DomainObjectException.class, NotAuthenticatedException.class, ResourceNotFoundException.class})
+    public ResponseEntity<CustomErrorResponse> handleCustomException
             (Exception exception, WebRequest request) {
 
+        String fieldName = getErrorMessageFieldName(exception);
         CustomErrorResponse errors = new CustomErrorResponse();
         Map<String, String> errorMessage = new HashMap<>();
-        errorMessage.put("email", exception.getMessage());
+        errorMessage.put(fieldName, exception.getMessage());
 
         errors.setTimestamp(LocalDateTime.now());
         errors.setStatus(HttpStatus.BAD_REQUEST.value());
@@ -38,17 +40,31 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    private String getErrorMessageFieldName(Exception exception) {
+        String fieldName = "error";
+        Class exceptionClass = exception.getClass();
+        if (exceptionClass.equals(DomainObjectException.class)) {
+            fieldName = "email";
+        } else if (exceptionClass.equals(NotAuthenticatedException.class)) {
+            fieldName = "user";
+        } else if (exceptionClass.equals(ResourceNotFoundException.class)) {
+            fieldName = "error";
+        }
+        return fieldName;
+    }
+
+    // error handle for @Valid
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid
-            (MethodArgumentNotValidException exception,
-             HttpHeaders headers, HttpStatus status, WebRequest request) {
+    (MethodArgumentNotValidException exception,
+     HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         Map<String, String> errorMessages = new HashMap<>();
         CustomErrorResponse errors = new CustomErrorResponse();
         BindingResult bindingResult = exception.getBindingResult();
 
         for (ObjectError objectError : bindingResult.getAllErrors()) {
-            String fieldName = ((FieldError)objectError).getField();
+            String fieldName = ((FieldError) objectError).getField();
             String defaultMessage = objectError.getDefaultMessage();
             errorMessages.put(fieldName, defaultMessage);
         }
@@ -60,9 +76,20 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(errors, headers, status);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public void constraintViolationException(HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value());
-    }
+    //  @Validate For Validating Path Variables and Request Parameters
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<CustomErrorResponse> handleConstraintViolation(
+            ConstraintViolationException ex, WebRequest request) {
+        Map<String, String> errorMessages = new HashMap<>();
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            errorMessages.put(violation.getPropertyPath().toString(), violation.getMessage());
+        }
 
+        CustomErrorResponse errors = new CustomErrorResponse();
+        errors.setTimestamp(LocalDateTime.now());
+        errors.setStatus(HttpStatus.BAD_REQUEST.value());
+        errors.setErrors(errorMessages);
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
