200631584815916672c9572ae9bbddf718923f69
HorabikJ <horabik.j@gmail.com>
2019-06-22 15:23:44 +0200

    Inserted new insert instructions in data.sql file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,12 @@ REPLACE INTO `user` VALUES (1, 1, 'admin@wp.pl', 'Admin', 'Admin', false ,'$2a$1
 REPLACE INTO `user_role` VALUES (1, 1);
 REPLACE INTO `user_role` VALUES (1, 2);
 
+
+-- insert regular user
+REPLACE INTO `user` VALUES (2, 1, 'jacek@wp.pl', 'Jacek', 'Horabik', true ,'$2a$10$G9tN3.tOA788nw1HyCyl9ui71y57MgP8w/rQz15Zo3BR7Gg5tSeq2');
+-- -- password for login: "jacek@wp.pl" is: "12345"
+REPLACE INTO `user_role` VALUES (2, 2);
+
 -- insert instructor
 INSERT INTO instructor (id, date_of_birth, description, email, first_name, last_name) VALUES (1, '1993-02-02', 'Fantastic trainer', 'trainer@wp.pl', 'Katarzyna', 'Jakubek');
 
