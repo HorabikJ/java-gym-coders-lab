5ff714d77c94d0a64c92e1bf0b9375373e6a97d5
HorabikJ <horabik.j@gmail.com>
2019-06-23 21:10:15 +0200

    Created InstructorService class and AdminInstructorController class.

---
+++ src/main/java/pl/coderslab/javaGym/controller/AdminInstructorController.java
@@ -0,0 +1,31 @@
+package pl.coderslab.javaGym.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.javaGym.entity.Instructor;
+import pl.coderslab.javaGym.service.InstructorService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/admin/instructor")
+@Validated
+public class AdminInstructorController {
+
+    private InstructorService instructorService;
+
+    @Autowired
+    public AdminInstructorController(InstructorService instructorService) {
+        this.instructorService = instructorService;
+    }
+
+//    TODO think about the same solution as in save User
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Instructor saveInstructor(@RequestBody @Valid Instructor instructor) {
+        return instructorService.save(instructor);
+    }
+
+}
---
+++ src/main/java/pl/coderslab/javaGym/service/InstructorService.java
@@ -0,0 +1,52 @@
+package pl.coderslab.javaGym.service;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import pl.coderslab.javaGym.email.EmailSender;
+import pl.coderslab.javaGym.entity.Instructor;
+import pl.coderslab.javaGym.error.customException.DomainObjectException;
+import pl.coderslab.javaGym.repository.InstructorRepository;
+
+import java.util.List;
+
+@Service
+public class InstructorService implements AbstractStandardService<Instructor> {
+
+    private InstructorRepository instructorRepository;
+    private EmailSender emailSender;
+
+    @Autowired
+    public InstructorService(InstructorRepository instructorRepository, EmailSender emailSender) {
+        this.instructorRepository = instructorRepository;
+        this.emailSender = emailSender;
+    }
+
+    @Override
+    public List<Instructor> findAll() {
+        return null;
+    }
+
+    @Override
+    public Instructor findById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public Instructor save(Instructor instructor) {
+        try {
+            if (instructor.getId() == null) {
+//                emailSender.sendEmail(instructor, EmailTypeEnum.WELCOME_INSTR_EMAIL);
+            }
+            return instructorRepository.save(instructor);
+        } catch (DataIntegrityViolationException | ConstraintViolationException e) {
+            throw new DomainObjectException();
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
