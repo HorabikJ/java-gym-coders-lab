1f4e1f1b6ac17dbd6cef7d747cebee0b6ac5e237
HorabikJ <horabik.j@gmail.com>
2019-06-23 10:39:42 +0200

    Updated DomainObjectException class.

--- src/main/java/pl/coderslab/javaGym/error/customException/DomainObjectException.java
+++ src/main/java/pl/coderslab/javaGym/error/customException/DomainObjectException.java
@@ -10,4 +10,12 @@ public class DomainObjectException extends RuntimeException {
         this.message = message;
     }
 
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
