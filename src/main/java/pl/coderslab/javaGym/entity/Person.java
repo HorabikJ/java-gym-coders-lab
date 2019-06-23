cd3d9b967710f3a2d5dce246f82d233d08e763ae
HorabikJ <horabik.j@gmail.com>
2019-06-23 18:54:57 +0200

    Added Person interface.

---
+++ src/main/java/pl/coderslab/javaGym/entity/Person.java
@@ -0,0 +1,21 @@
+package pl.coderslab.javaGym.entity;
+
+public interface Person {
+
+    Long getId();
+
+    void setId(Long id);
+
+    String getEmail();
+
+    void setEmail(String email);
+
+    String getFirstName();
+
+    void setFirstName(String firstName);
+
+    String getLastName();
+
+    void setLastName(String lastName);
+
+}
